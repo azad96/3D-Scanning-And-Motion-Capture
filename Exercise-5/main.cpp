947256a39bb748cc5267e92257fa75f1e5206865
Azad <azad.baykara96@gmail.com>
2022-11-28 20:09:53 +0100

    macros updated

--- Exercise-5/main.cpp
+++ Exercise-5/main.cpp
@@ -7,13 +7,13 @@
 #include "ICPOptimizer.h"
 #include "PointCloud.h"
 
-#define SHOW_BUNNY_CORRESPONDENCES 1
+#define SHOW_BUNNY_CORRESPONDENCES 0
 
 #define USE_POINT_TO_PLANE	0
-#define USE_LINEAR_ICP		0
+#define USE_LINEAR_ICP		1
 
 #define RUN_SHAPE_ICP		1
-#define RUN_SEQUENCE_ICP	1
+#define RUN_SEQUENCE_ICP	0
 
 void debugCorrespondenceMatching() {
 	// Load the source and target mesh.
